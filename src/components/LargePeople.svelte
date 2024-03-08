349644e9456ac3370a306374330d55ebb1add7c4
moonish1211 <jillnomura1211@gmail.com>
2024-03-08 01:42:39 -0800

    'minor_change'

--- src/components/LargePeople.svelte
+++ src/components/LargePeople.svelte
@@ -86,8 +86,12 @@ function createButton_in() {
       .append('button') // Append a button element
       .text('Scope in') // Set the text content of the button
       .on('click', handleClick_in) // Add a click event listener
-      .style('font-size', '18px') // Set the font size
-      .style('font-family', 'Arial, sans-serif'); // Set the font family
+      .style('font-size', '22px') // Set the font size
+      .style('font-family', 'VT323, monospace') // Set the font family
+      .style('width', '130px') // Set the width of the button
+      .style('height', '50px') // Set the height of the button
+      .style('background-color', 'beige'); // Set the background color of the button
+
   }
 
 function createButton_out() {
@@ -95,8 +99,11 @@ function createButton_out() {
       .append('button') // Append a button element
       .text('Scope out') // Set the text content of the button
       .on('click', handleClick_out) // Add a click event listener
-      .style('font-size', '18px') // Set the font size
-      .style('font-family', 'Arial, sans-serif'); // Set the font family
+      .style('font-size', '22px') // Set the font size
+      .style('font-family', 'VT323, monospace') // Set the font family
+      .style('width', '130px') // Set the width of the button
+      .style('height', '50px') // Set the height of the button
+      .style('background-color', 'beige'); // Set the background color of the button
   }
 
 onMount(() => {
@@ -119,7 +126,7 @@ onMount(() => {
     For example, when you see 500K views on TikTok, we all agree that it is a large number, but have you ever imagined how much 500K of people look like? It doesn't even fit a largest stadium in the world!
     <br>
     These numbers can scale up really quickly, but when you put into perspective that each 1s that make up these huge numbers are actual human, we have difficulties understanding them. 
-    <br>
+    <br><br>
     This visualization below is intended to show you how much these numbers of people can scale up to give you a better understanding of how large population can look like. 
 </p>
 
@@ -130,9 +137,9 @@ onMount(() => {
 </div>
 
 <svg {width} {height} viewBox="0 0 {width} {height}">
-    <g stroke="black" stroke-width="0.5" transform="translate(120, 80)" class = "layer" id = 'Row1'>
+    <g stroke="black" stroke-width="0.5" transform="translate(300, 40)" class = "layer" id = 'Row1'>
         <text transform="translate(100, 50)" font-size="75">{firstRow['Stat']}</text>
-        <text font-size="25" transform="translate(0, 80)"> Description: {firstRow['Data_descipt']}</text>
+        <text font-size="25" transform="translate(0, 90)"> Description: {firstRow['Data_descipt']}</text>
     </g>
     <g stroke="black" stroke-width="0.5" transform="translate(0,40)" class = "layer" id = 'Row2'>
         <g transform="translate(30, 0)">
@@ -148,7 +155,7 @@ onMount(() => {
         <text font-size="25" transform="translate(0, 220)"> Description: {secondRow['Data_descipt']}</text>
     </g>
     <g stroke="black" stroke-width="0.5" class = "layer" id = 'Row3'>
-        <g transform="translate(30, 60)" font-size="20">
+        <g transform="translate(30, 40)" font-size="20">
             {#each iterations5 as i, indexh}
                 {#each iterations6 as j, index}
                     <text fill="blue" x={index * spacingwidth} y={indexh * spacingheight}>{secondRow['Stat']}</text>
@@ -156,14 +163,14 @@ onMount(() => {
             {/each}
         </g>
         <g>
-            <text font-size="50" transform="translate(400, 160)"> = </text>
-            <text font-size="40" transform="translate(450, 130)" fill="brown"> 30 rooms of DSC 106 student watching demo</text>
-            <text font-size="40" transform="translate(450, 190)" fill="brown"> 3K people</text>
+            <text font-size="50" transform="translate(400, 150)"> = </text>
+            <text font-size="40" transform="translate(450, 120)" fill="brown"> 30 rooms of DSC 106 student watching demo</text>
+            <text font-size="40" transform="translate(450, 180)" fill="brown"> 3K people</text>
         </g>
-        <text font-size="25" transform="translate(0, 295)"> Description: {thirdRow['Data_descipt']}</text>
+        <text font-size="25" transform="translate(0, 285)"> Description: {thirdRow['Data_descipt']}</text>
     </g>
     <g stroke="black" stroke-width="0.5" class = "layer" id = 'Row4'>
-        <g transform="translate(10, 80)" font-size="25">
+        <g transform="translate(10, 50)" font-size="25">
             {#each iterations4 as i, indexh}
                 {#each iterations6 as j, index}
                     <text fill="brown" x={index * 65} y={indexh * spacingheight}>{thirdRow['Stat']}</text>
@@ -171,14 +178,14 @@ onMount(() => {
             {/each}
         </g>
         <g>
-            <text font-size="50" transform="translate(400, 160)"> = </text>
-            <text font-size="40" transform="translate(450, 130)" fill="green"> 24 twitch streamer with 3000 views</text>
-            <text font-size="40" transform="translate(450, 190)" fill="green"> 70K people</text>
+            <text font-size="50" transform="translate(400, 130)"> = </text>
+            <text font-size="40" transform="translate(450, 100)" fill="green"> 24 twitch streamer with 3000 views</text>
+            <text font-size="40" transform="translate(450, 160)" fill="green"> 70K people</text>
         </g>
-        <text font-size="25" transform="translate(0, 280)"> Description: {forthRow['Data_descipt']}</text>
+        <text font-size="25" transform="translate(0, 240)"> Description: {forthRow['Data_descipt']}</text>
     </g>
     <g stroke="black" stroke-width="0.5" class = "layer" id = 'Row5'>
-        <g transform="translate(0, 80)" font-size="25">
+        <g transform="translate(10, 50)" font-size="25">
             {#each iterations4 as i, indexh}
                 {#each iterations5 as j, index}
                     <text fill="green" x={index * 80} y={indexh * spacingheight}>{forthRow['Stat']}</text>
@@ -186,11 +193,11 @@ onMount(() => {
             {/each}
         </g>
         <g>
-            <text font-size="50" transform="translate(400, 160)"> = </text>
-            <text font-size="40" transform="translate(450, 130)" fill="red"> 15 Sofi Stadium</text>
-            <text font-size="40" transform="translate(450, 190)" fill="red"> 1.3M  people</text>
+            <text font-size="50" transform="translate(410, 130)"> = </text>
+            <text font-size="40" transform="translate(460, 100)" fill="red"> 15 Sofi Stadium</text>
+            <text font-size="40" transform="translate(460, 160)" fill="red"> 1.3M  people</text>
         </g>
-        <text font-size="25" transform="translate(0, 280)"> Description: {fifthRow['Data_descipt']}</text>
+        <text font-size="25" transform="translate(10, 240)"> Description: {fifthRow['Data_descipt']}</text>
     </g>
 </svg>
 
@@ -198,12 +205,14 @@ onMount(() => {
 <style>
 .buttons {
   display: flex; /* Use flexbox layout */
+  margin-top: 10px;
+  margin-left : 240px;
 }
 
 .zoom_in{
-    width: 100px; /* Set width of each button */
-    height: 40px; /* Set height of each button */
-    margin-right: 10px; /* Add some margin between buttons */
+    width: 150px; /* Set width of each button */
+    height: 70px; /* Set height of each button */
+    margin-right: 50px; /* Add some margin between buttons */
     justify-content: center; /* Center content horizontally */
     align-items: center; /* Center content vertically */
     display: flex; /* Use flexbox layout for buttons */
@@ -211,8 +220,8 @@ onMount(() => {
     
 }
 .zoom_out {
-    width: 100px; /* Set width of each button */
-    height: 40px; /* Set height of each button */
+    width: 150px; /* Set width of each button */
+    height: 70px; /* Set height of each button */
     margin-right: 14px; /* Add some margin between buttons */
     display: flex; /* Use flexbox layout for buttons */
     justify-content: center; /* Center content horizontally */
