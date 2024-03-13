ace854b926cbdb84c5372b1def92eff0d1c0c73e
moonish1211 <jillnomura1211@gmail.com>
2024-03-13 00:35:15 -0700

    'change_highlights'

--- src/components/LargePeople.svelte
+++ src/components/LargePeople.svelte
@@ -111,6 +111,7 @@ onMount(() => {
     createButton_out();
   });
 
+
 </script>
 <br>
 <h2>Part 1: Visualizing large numbers of peoples</h2>
@@ -137,12 +138,13 @@ onMount(() => {
 
 <div class = 'content'>
 <svg {width} {height} viewBox="0 0 {width} {height}">
-    <g stroke="black" stroke-width="0.5" transform="translate(530, 40)" class = "layer" id = 'Row1'>
+    <g stroke="black" stroke-width="0.5" transform="translate(0, 100)" class = "layer" id = 'Row1'>
         <text transform="translate(100, 50)" font-size="75">{firstRow['Stat']}</text>
         <text font-size="25" transform="translate(0, 90)"> Description: {firstRow['Data_descipt']}</text>
     </g>
-    <g stroke="black" stroke-width="0.5" transform="translate(330,40)" class = "layer" id = 'Row2'>
+    <g stroke="black" stroke-width="0.5" transform="translate(0,40)" class = "layer" id = 'Row2'>
         <g transform="translate(30, 0)">
+            let first = 1;
             {#each iterations10 as i, indexh}
                 {#each iterations10 as j, index}
                     <text x={index * spacingwidth_1} y={indexh * spacingheight_1}>{firstRow['Stat']}</text>
@@ -154,48 +156,59 @@ onMount(() => {
         </g>
         <text font-size="25" transform="translate(0, 220)"> Description: {secondRow['Data_descipt']}</text>
     </g>
-    <g stroke="black" stroke-width="0.5" class = "layer" id = 'Row3' transform="translate(230,0)">
+    <g stroke="black" stroke-width="0.5" class = "layer" id = 'Row3' transform="translate(0,0)">
         <g transform="translate(30, 40)" font-size="20">
             {#each iterations5 as i, indexh}
                 {#each iterations6 as j, index}
-                    <text fill="blue" x={index * spacingwidth} y={indexh * spacingheight}>{secondRow['Stat']}</text>
+                    <!-- <text fill="blue" x={index * spacingwidth} y={indexh * spacingheight}>{secondRow['Stat']}</text> -->
+                    {#if i === 1 && j === 1}
+                        <text id = "scoping"fill="orange" x={index * spacingwidth} y={indexh * spacingheight}>{secondRow['Stat']}</text>
+                    {:else}
+                        <text fill="blue" x={index * spacingwidth} y={indexh * spacingheight}>{secondRow['Stat']}</text>
+                    {/if}
+
                 {/each}
             {/each}
         </g>
         <g>
             <text font-size="50" transform="translate(400, 150)"> = </text>
-            <text font-size="40" transform="translate(450, 120)" fill="brown"> 30 rooms of DSC 106 student</text>
-            <text font-size="40" transform="translate(450, 180)" fill="brown"> 3K people</text>
+            <text font-size="40" transform="translate(450, 150)" fill="brown"> 3K people</text>
         </g>
         <text font-size="25" transform="translate(0, 285)"> Description: {thirdRow['Data_descipt']}</text>
     </g>
-    <g stroke="black" stroke-width="0.5" class = "layer" id = 'Row4' transform="translate(230,0)">
+    <g stroke="black" stroke-width="0.5" class = "layer" id = 'Row4' transform="translate(0,0)">
         <g transform="translate(10, 50)" font-size="25">
             {#each iterations4 as i, indexh}
                 {#each iterations6 as j, index}
-                    <text fill="brown" x={index * 65} y={indexh * spacingheight}>{thirdRow['Stat']}</text>
+                    {#if i === 1 && j ===1}
+                        <text id = "scoping" fill="blue" x={index * 65} y={indexh * spacingheight}>{thirdRow['Stat']}</text>
+                    {:else}
+                        <text fill="brown" x={index * 65} y={indexh * spacingheight}>{thirdRow['Stat']}</text>
+                    {/if}
                 {/each}
             {/each}
         </g>
         <g>
             <text font-size="50" transform="translate(400, 130)"> = </text>
-            <text font-size="40" transform="translate(450, 100)" fill="green"> 24 twitch streamer with 3000 views</text>
-            <text font-size="40" transform="translate(450, 160)" fill="green"> 70K people</text>
+            <text font-size="40" transform="translate(450, 130)" fill="green"> 70K people</text>
         </g>
         <text font-size="25" transform="translate(0, 240)"> Description: {forthRow['Data_descipt']}</text>
     </g>
-    <g stroke="black" stroke-width="0.5" class = "layer" id = 'Row5' transform="translate(330,0)">
+    <g stroke="black" stroke-width="0.5" class = "layer" id = 'Row5' transform="translate(0,0)">
         <g transform="translate(10, 50)" font-size="25">
             {#each iterations4 as i, indexh}
                 {#each iterations5 as j, index}
-                    <text fill="green" x={index * 80} y={indexh * spacingheight}>{forthRow['Stat']}</text>
+                    {#if i === 1 && j===1}
+                        <text id = "scoping" fill="rgba(245, 40, 145, 0.8)" x={index * 80} y={indexh * spacingheight}>{forthRow['Stat']}</text>
+                    {:else}
+                        <text fill="green" x={index * 80} y={indexh * spacingheight}>{forthRow['Stat']}</text>
+                    {/if}
                 {/each}
             {/each}
         </g>
         <g>
             <text font-size="50" transform="translate(410, 130)"> = </text>
-            <text font-size="40" transform="translate(460, 100)" fill="red"> 15 Sofi Stadium</text>
-            <text font-size="40" transform="translate(460, 160)" fill="red"> 1.3M  people</text>
+            <text font-size="40" transform="translate(460, 130)" fill="red"> 1.3M  people</text>
         </g>
         <text font-size="25" transform="translate(10, 240)"> Description: {fifthRow['Data_descipt']}</text>
     </g>
