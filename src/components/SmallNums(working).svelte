97f69659d498703535a596d6086d3f7d26c311e1
hunterbrownell <77085663+hunterbrownell@users.noreply.github.com>
2024-03-14 11:04:14 -0700

    Update SmallNums(working).svelte
    Buttons and Aesthetic updates

--- src/components/SmallNums(working).svelte
+++ src/components/SmallNums(working).svelte
@@ -16,19 +16,12 @@
     ctx.moveTo(0, 100);
     ctx.lineTo(1200, 100);
     ctx.lineWidth = 200;
-    ctx.strokeStyle = '#8B4513'; 
-    ctx.stroke();
-
-    ctx.beginPath();
-    ctx.moveTo(0, 200);
-    ctx.lineTo(1200, 200);
-    ctx.lineWidth = 50;
-    ctx.strokeStyle = 'orange'; 
+    ctx.strokeStyle = '#B8833F'; 
     ctx.stroke();
 
     ctx.font = '20px Arial';
     ctx.fillStyle = 'black';
-    ctx.fillText(`Width of human hair ➗ ${zoom}`, 500, 195);
+    ctx.fillText(`Width of human hair ➗ ${zoom}`, 500, 160);
 
     const numberOfMolecules = (hairWidthNm/waterMoleculeLengthNm)/zoom
     const step = 1200/numberOfMolecules
@@ -40,6 +33,28 @@
       ctx.fillStyle = 'blue';
       ctx.fill();
     }
+
+    ctx.beginPath();
+    ctx.moveTo(0, 180); 
+    ctx.lineTo(1200, 180);
+    ctx.lineWidth = 5;
+    ctx.strokeStyle = 'black'; 
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(2, 160); 
+    ctx.lineTo(2, 200); 
+    ctx.lineWidth = 5;
+    ctx.stroke();
+
+    ctx.beginPath();
+    ctx.moveTo(1198, 160); 
+    ctx.lineTo(1198, 200);
+    ctx.lineWidth = 5;
+    ctx.stroke();  
+  }
+  function setZoom(level) {
+    zoom = level;
   }
 
   onMount(() => {
@@ -53,8 +68,10 @@
 </script>
 
 <div>
-    <label for="zoom">Zoom:</label>
-    <input type="range" id="zoom" min="1" max="10000" value={zoom} on:input="{e => zoom = +e.target.value}" />
-  
+  <button on:click="{() => setZoom(100)}">Zoom 100x</button>
+  <button on:click="{() => setZoom(5000)}">Zoom 5,000x</button>
+  <button on:click="{() => setZoom(10000)}">Zoom 10,000x</button>
+  <label for="zoom">Zoom:</label>
+  <input type="range" id="zoom" min="1" max="10000" value={zoom} on:input="{e => zoom = +e.target.value}" />
 </div>
 <canvas id="visualizationCanvas" width="1200" height="200" style="border: 1px solid black;"></canvas>
